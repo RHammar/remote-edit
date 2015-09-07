668081a00d95ef8d470c8e3a9f57df1bf25ec48a
Rickard Hammar <rickardh@axis.com>
2015-09-07 09:58:52 +0200

    moved path mapping to settings

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # remote-edit
+Sublime-text-3 plugin to scp files to a remote server when saveing a file.
+Requires that you have previously copied your ssh-key to the remote machine
+using ssh-copy-id.
--- remote-edit.py
+++ remote-edit.py
@@ -1,29 +1,30 @@
 import sublime, sublime_plugin, os
 
-#host='sts@exploratory9-avhs2.vhslab.rnd.axis.com'
-# host='sts@DebianAVHS'
 host = None
+path_mapping = None
 def plugin_loaded():
   s = sublime.load_settings('remote-edit.sublime-settings')
   s.add_on_change('readsettings', update_settings)
   update_settings()
 
 def update_settings():
-    print("update_settings")
-    global host
+    global host, path_mapping
     s = sublime.load_settings('remote-edit.sublime-settings')
     host = s.get('host')
+    path_mapping = s.get('path_mapping')
+    # print(repr(path_mapping))
 
 class RemoteEdit(sublime_plugin.EventListener):
     def on_post_save(self, view):
-        remote = {"/home/rickardh/repos/avhsportal/avhs/webgui": "/usr/bin/scp '$1' "+ host +":'~/current/web/webgui/$2'",
-                  "/home/rickardh/repos/avhsportal/avhs/portal": "/usr/bin/scp '$1' "+ host +":'~/current/$2'",
-                  "/home/rickardh/repos/avhsportal/avhs/portal/apps/helpers": "/usr/bin/scp '$1' "+ host +":'~/current/bin/$2'",
-                  "/home/rickardh/repos/webgrind":               "/usr/bin/scp '$1' "+ host +":'~/current/web/webgrind/$2'"}
-                  # "/home/rickardh/script/explorer.html": "/usr/bin/scp '$1' sts@DebianAVHS:'~/current/web/webgui/$2'"}
 
-        for dirname, target in remote.items():
+        copy = {}
+        for local,remote in path_mapping.items():
+            copy[local] = "/usr/bin/scp '$1' "+ host +":'" + remote + "$2'"
+
+        # print(repr(copy))
+        for dirname, target in copy.items():
             if view.file_name().startswith(dirname):
+                print(len(dirname))
                 target = target.replace("$1", view.file_name())
                 target = target.replace("$2", view.file_name()[len(dirname):])
 
