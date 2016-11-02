ccd26bf9571b314771fae0188623bd04a44985a7
Rickard Hammar <rickardh@axis.com>
2016-11-02 14:49:50 +0100

    Support multiple hosts

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # remote-edit
-Sublime-text-3 plugin to scp files to a remote server when saveing a file.
+Sublime-text-3 plugin to scp files to a remote server when saving a file.
 Requires that you have previously copied your ssh-key to the remote machine
 using ssh-copy-id.
+For example:
+
+```ssh-copy-id user@myhost1```
+
+Supports copying to multiple hosts, given that the files are located on the same place.
--- remote-edit.py
+++ remote-edit.py
@@ -1,6 +1,6 @@
 import sublime, sublime_plugin, os
 
-host = None
+hosts = None
 path_mapping = None
 def plugin_loaded():
   s = sublime.load_settings('remote-edit.sublime-settings')
@@ -8,25 +8,28 @@ def plugin_loaded():
   update_settings()
 
 def update_settings():
-    global host, path_mapping
+    global hosts, path_mapping
     s = sublime.load_settings('remote-edit.sublime-settings')
-    host = s.get('host')
+    hosts = s.get('hosts')
     path_mapping = s.get('path_mapping')
     # print(repr(path_mapping))
 
 class RemoteEdit(sublime_plugin.EventListener):
     def on_post_save(self, view):
 
-        copy = {}
-        for local,remote in path_mapping.items():
-            copy[local] = "/usr/bin/scp '$1' "+ host +":'" + remote + "$2'"
+        print(hosts)
+        for host in hosts:
+            copy = {}
+            # print('##############', host, '##############')
 
-        # print(repr(copy))
-        for dirname, target in copy.items():
-            if view.file_name().startswith(dirname):
-                print(len(dirname))
-                target = target.replace("$1", view.file_name())
-                target = target.replace("$2", view.file_name()[len(dirname):])
+            for local,remote in path_mapping.items():
+                copy[local] = "/usr/bin/scp '$1' "+ host +":'" + remote + "$2'"
 
-                print("remote edit: " + target)
-                os.system(target + " &")
+            # print(repr(copy))
+            for dirname, target in copy.items():
+                if view.file_name().startswith(dirname):
+                    target = target.replace("$1", view.file_name())
+                    target = target.replace("$2", view.file_name()[len(dirname):])
+
+                    print("remote edit: " + target)
+                    os.system(target + " &")
